d1b4758930052e0d8e46ccf25b869884297b3599
CapsMussa <nzbarec@inbox.ru>
2022-11-30 11:19:24 +0500

    First Commit

--- resources/views/page/header.blade.php
+++ resources/views/page/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <link rel="stylesheet" href="css/app.css">
     <h1>Street Cross.</h1>
-    <nav><a href="home">Акции</a><a href="katalog">Каталог</a><a>Детям</a><a>Корзина</a></nav>
+    <nav><a href="/">Акции</a><a href="katalog">Каталог</a><a>Детям</a><a>Корзина</a></nav>
 </header>
 <body>
 @yield('catalog')
